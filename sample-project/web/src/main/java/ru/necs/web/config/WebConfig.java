3491b96f4ac165a5ff7b72db59a03681db19a9e3
Dmitry <koneworks@gmail.com>
2017-10-03 13:21:47 +0500

    добавили правильную конфигурацию

--- sample-project/web/src/main/java/ru/necs/web/config/WebConfig.java
+++ sample-project/web/src/main/java/ru/necs/web/config/WebConfig.java
@@ -5,10 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import ru.necs.domain.spring.DomainConfig;
+import ru.necs.web.controller.spi.SPIConfig;
 
 @EnableWebMvc
 @Configuration
-@Import(DomainConfig.class)
+@Import({DomainConfig.class,SPIConfig.class})
 @ComponentScan({"ru.necs.web.controller", "ru.necs.web.orika"})
 public class WebConfig {
 
--- sample-project/web/src/main/java/ru/necs/web/controller/spi/SPIController.java
+++ sample-project/web/src/main/java/ru/necs/web/controller/spi/SPIController.java
@@ -1,5 +1,7 @@
 package ru.necs.web.controller.spi;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,4 +12,7 @@ public abstract class SPIController {
 
 	@Autowired
 	protected ConfigService service;
+	
+	@PostConstruct
+	public abstract void postInit();
 }
