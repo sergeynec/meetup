e52de86d91b9cbff3cffeb9712e38457fcddb90c
Dmitry <koneworks@gmail.com>
2017-10-04 00:34:42 +0500

    ending

--- sample-project/web/src/main/java/ru/necs/web/controller/spi/SPIConfig.java
+++ sample-project/web/src/main/java/ru/necs/web/controller/spi/SPIConfig.java
@@ -1,16 +1,31 @@
 package ru.necs.web.controller.spi;
 
 import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.serviceloader.ServiceListFactoryBean;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextClosedEvent;
+
+import ru.necs.domain.service.ConfigService;
 
 @Configuration
 public class SPIConfig {
 
+	@Autowired
+	private ConfigurableApplicationContext context;
+	
+	
 	@Bean
 	public ServiceListFactoryBean serviceBeans() {
 		ServiceListFactoryBean serviceListFactoryBean = new ServiceListFactoryBean();
@@ -19,13 +34,18 @@ public class SPIConfig {
 	}
 	
 	@Bean 
-	public SPIController getSpiController(ServiceListFactoryBean serviceListFactoryBean) throws Exception {
-		List<?> spiServices  = (List<?>) serviceListFactoryBean.getObject();
-		if (!Validate.notNull(spiServices).isEmpty() && spiServices.get(0) instanceof SPIController) {
-			return (SPIController) spiServices.get(0);	
-		} else {
-			throw new NoSuchBeanDefinitionException(SPIController.class);
-		}
+	public List<SPIController> getSpiController(ServiceListFactoryBean serviceListFactoryBean, ConfigService service) throws Exception {
+		List<SPIController> spiServices  = (List<SPIController>) serviceListFactoryBean.getObject();
+		spiServices.forEach(spi->spi.init(service));
+		context.addApplicationListener(new ApplicationListener<ContextClosedEvent>() {
+
+			@Override
+			public void onApplicationEvent(ContextClosedEvent event) {
+				spiServices.forEach(spi->spi.destroy());
+				
+			}
+		});
+		return spiServices;	
 		
 	}
 }
--- sample-project/web/src/main/java/ru/necs/web/controller/spi/SPIController.java
+++ sample-project/web/src/main/java/ru/necs/web/controller/spi/SPIController.java
@@ -1,18 +1,10 @@
 package ru.necs.web.controller.spi;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import ru.necs.domain.service.ConfigService;
 
-@Component
 public abstract class SPIController {
 
-	@Autowired
-	protected ConfigService service;
-	
-	@PostConstruct
-	public abstract void postInit();
+	public abstract void init(ConfigService service);
+
+	public abstract void destroy();
 }
