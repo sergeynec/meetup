7a85a7b7aa42d7ed9ccbfc1e6ec1cbcc2093e0e5
Dmitry <koneworks@gmail.com>
2017-10-04 11:22:15 +0500

    power lambda

--- sample-project/web/src/main/java/ru/necs/web/controller/spi/SPIConfig.java
+++ sample-project/web/src/main/java/ru/necs/web/controller/spi/SPIConfig.java
@@ -1,17 +1,9 @@
 package ru.necs.web.controller.spi;
 
 import java.util.List;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
-import org.apache.commons.lang3.Validate;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.serviceloader.ServiceListFactoryBean;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.GenericBeanDefinition;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,19 +25,12 @@ public class SPIConfig {
 		return serviceListFactoryBean;
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Bean 
 	public List<SPIController> getSpiController(ServiceListFactoryBean serviceListFactoryBean, ConfigService service) throws Exception {
 		List<SPIController> spiServices  = (List<SPIController>) serviceListFactoryBean.getObject();
 		spiServices.forEach(spi->spi.init(service));
-		context.addApplicationListener(new ApplicationListener<ContextClosedEvent>() {
-
-			@Override
-			public void onApplicationEvent(ContextClosedEvent event) {
-				spiServices.forEach(spi->spi.destroy());
-				
-			}
-		});
+		context.addApplicationListener((ContextClosedEvent event)->spiServices.forEach(spi->spi.destroy()));
 		return spiServices;	
-		
 	}
 }
